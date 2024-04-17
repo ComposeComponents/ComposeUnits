b2cbc4e4971a90e1a156960897dd14e74f9a3c53
Ben McLean <benmclean007@gmail.com>
2024-04-17 12:09:27 +1000

    Setup initial library

---
+++ units/src/main/java/cl/emilym/compose/units/Px.kt
@@ -0,0 +1,16 @@
+package cl.emilym.compose.units
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+val Double.px: Dp
+    @Composable
+    get() = with(LocalDensity.current) { this@px.toFloat().toDp() }
+val Float.px: Dp
+    @Composable
+    get() = with(LocalDensity.current) { this@px.toDp() }
+val Int.px: Dp
+    @Composable
+    get() = with(LocalDensity.current) { this@px.toDp() }
